d0843e5b8cab33dd24fa6c463952a83837f26d09
kryz <k@kryz.dev>
2022-09-09 15:11:07 +0200

    conn outpur

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "dev_ip" {
   value = aws_instance.dev_node.public_ip
 }
+
+output "conn_script" {
+  value = templatefile("${var.host_os}-ssh-config.tpl", {
+    hostname     = aws_instance.dev_node.public_ip,
+    user         = "ubuntu",
+    identityfile = "~/.ssh/id_rsa"
+  })
+}
